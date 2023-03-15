9a6eafb0ccb740714282c964a23d96c5a11d70b3
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2023-03-15 12:34:53 -0400

    Add files via upload

---
+++ instaparsep2-main/lab-insta-parse/Extensions/DateFormatter+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  DateFormatter+Extensions.swift
+//  lab-insta-parse
+//
+//  Created by Chloe Posthuma-Coelho
+
+import Foundation
+
+extension DateFormatter {
+    static var postFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        return formatter
+    }()
+}
